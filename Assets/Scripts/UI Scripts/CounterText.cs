afab849e53400eb6f2e403f806eaaea48c7cba7d
Xurya <xur1@bxscience.edu>
2020-08-01 22:04:12 -0400

    UI pull req from maze :P

---
+++ Assets/Scripts/UI Scripts/CounterText.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+
+public class CounterText : MonoBehaviour//, IOnEventCallback
+{
+    // public GameObject player;
+    // public Text pointCounter;
+    // public GameObject winnerText;
+    // public const byte GameOverCode = 1;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     int points = player.GetComponent<PlayerController>().points;
+    //     pointCounter.text = points.ToString();
+    //     if(points >= 3){
+    //         winnerText.SetActive(true);
+    //         winnerText.GetComponent<Text>().text = "Congratulations! You got the most toppings!\nWinner";
+    //         onGameEnd();
+    //     }
+    // }
+
+    // private void OnEnable(){
+    //     PhotonNetwork.AddCallbackTarget(this);
+    // }
+
+    // private void OnDisable(){
+    //     PhotonNetwork.RemoveCallbackTarget(this);
+    // }
+
+    // private void onGameEnd(){
+    //     RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All }; // You would have to set the Receivers to All in order to receive this event on the local client as well
+    //     PhotonNetwork.RaiseEvent(GameOverCode, PUN2_RoomController.getNickname(), raiseEventOptions, SendOptions.SendReliable);
+    //     PhotonNetwork.RemoveCallbackTarget(this);
+    // }
+
+    // public void OnEvent(EventData photonEvent) {
+    //     byte eventCode = photonEvent.Code;
+    //     if(eventCode == GameOverCode){
+    //         Debug.Log("ROAD ROLLA DA");
+    //         StartCoroutine(waiter());
+    //         PUN2_RoomController.leaveRoom();
+    //     }
+    // }
+
+    // IEnumerator waiter(){
+    //     yield return new WaitForSeconds(5f);
+    // }
+}
