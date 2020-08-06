5d00198fb8da23da4a224d86f9163894256be995
Jeffrey Weng <46985822+jwengbing@users.noreply.github.com>
2020-08-05 21:55:44 -0400

    collider wip2

--- Assets/Scripts/CTP.cs
+++ Assets/Scripts/CTP.cs
@@ -1,19 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CTP : MonoBehaviour
 {
-    public GameObject point;
     public float CatTime = 0;
     public float DogTime = 0;
+    public int catCount = 0;
+    public int dogCount = 0;
     public bool cat = false;
     public bool dog = false;
     public int captureTime = 60;
     // Start is called before the first frame update
     void Start()
     {
-        
-      
-
+        checkColliders();
+        Debug.Log(" cats: " + catCount + " dogs : " + dogCount);
     }
 
     // Update is called once per frame
@@ -21,15 +22,25 @@ public class CTP : MonoBehaviour
     {
         //get colliders 
         //check if dog > cat colliders, if so call update
-
+        updateTime();
+        
+        Debug.Log(dog);
+        Debug.Log(DogTime);
+        Debug.Log(CatTime);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void checkColliders()
     {
-        if (collision.gameObject.tag == "cat") setCat();
-        else if (collision.gameObject.tag == "dog") setDog();
+        Collider[] colliders = Physics.OverlapBox(this.transform.position, transform.localScale);
+        for(int i = 0; i< colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.tag == "Dog" || colliders[i].gameObject.tag == "Cat")
+            {
+                if (colliders[i].gameObject.tag == "Dog") dogCount++;
+                else if (colliders[i].gameObject.tag == "Cat") catCount++;
+            }
+        }
     }
-
     void setCat()
     {
         cat = true;
@@ -44,13 +55,13 @@ public class CTP : MonoBehaviour
     {
         if (cat)
         {
-            if (DogTime > CatTime) DogTime -= Time.deltaTime;
+            if (DogTime > CatTime && dogCount > catCount) DogTime -= Time.deltaTime;
             else CatTime += Time.deltaTime;
         }
 
         else if (dog)
         {
-            if (CatTime > DogTime) CatTime -= Time.deltaTime;
+            if (CatTime > DogTime && catCount > dogCount) CatTime -= Time.deltaTime;
             else DogTime += Time.deltaTime;
         }
     }
