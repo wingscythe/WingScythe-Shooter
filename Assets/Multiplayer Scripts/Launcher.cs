7df1acbad410bf27caae40107bb6beaa9619da5f
Eric Tong <eric.tong105@gmail.com>
2020-08-07 19:26:25 -0400

    Lobby Multiplayer

--- Assets/Multiplayer Scripts/Launcher.cs
+++ Assets/Multiplayer Scripts/Launcher.cs
@@ -2,79 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
     //# symbol is for pre-processing directives
     //# symbol always starts with #___ and ends with #end___
-    string gameVersion = "1";
+    //Level Name
+    public string levelName = "Shooter";
+    public int maxPlayers = 4;
 
+    //Start menu UI integration
     [Tooltip("The Ui Panel to let the user enter name, connect and play")]
     [SerializeField]
     private GameObject startMenu;
-    [Tooltip("The UI Label to inform the user that the connection is in progress")]
-    [SerializeField]
-    private GameObject loadingLabel;
+    // [Tooltip("The UI Label to inform the user that the connection is in progress")]
+    // [SerializeField]
+    // private GameObject loadingLabel;
 
-
-
-
-    //Monobehaviour called before Start()
-    void Awake() 
-    {
-        //This makes sure we can use PhotonNetwork.LoadLevel() 
-        PhotonNetwork.AutomaticallySyncScene = true;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Connect();
-    }
-
-    /// Start the connection process
-    /// - If already connected, join random room
-    /// - If not, connect player to the instance of the Photon Cloud Network created for this game
-    public void Connect() 
-    {
-        // Check if connected
-        if (PhotonNetwork.IsConnected)
-        {
-            // Join random room
-            PhotonNetwork.JoinRandomRoom();
-        }
-        else
-        {
-            //Connect to instance of Photon Cloud Network
-            PhotonNetwork.ConnectUsingSettings();
-            PhotonNetwork.GameVersion = gameVersion;
-        }
-    }
-
-    //Checks if player was connected / disconnected
-    // - If player was connected make player join random room
-    // - If not, send a message/cause
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("OnConnectedToMaster Called");
-        PhotonNetwork.JoinRandomRoom();
-    }
-
-    public override void OnDisconnected(Photon.Realtime.DisconnectCause cause)
-    {
-        Debug.LogWarningFormat("OnDisconnected Called", cause);
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        Debug.Log("JoinRandomFailed");
-        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = maxPlayersPerRoom });
-    }
-
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("OnJoinedRoom Called");
-    }
+    //Player name
+    string playerName = "Player 1";
+    //User is separated from each other by game versions
+    string gameVersion = "0.9";
+    //List of created rooms
+    List<RoomInfo> createdRooms = new List<RoomInfo>();
 
     //Serialized to expose non public field
     //Non public so it can only be modified in inspector and not in other scripts
@@ -82,9 +33,190 @@ public class Launcher : MonoBehaviourPunCallbacks
     [SerializeField]
     private byte maxPlayersPerRoom = 4;
 
-   /* public override void CreateRoom()
-    {
+    //Use this name when creating a room
+    string roomName = "Room 1";
+    Vector2 roomListScroll = Vector2.zero;
+    bool joiningRoom = false;
 
+
+    //Monobehaviour called before Start()
+    void Awake () {
+        //This makes sure we can use PhotonNetwork.LoadLevel() 
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
-   */
+
+    // Start is called before the first frame update
+    void Start () {
+        Connect();
+    }
+
+    /// Start the connection process
+    /// - If already connected, join random room
+    /// - If not, connect player to the instance of the Photon Cloud Network created for this game
+    public void Connect () {
+        // Check if connected
+        if (!PhotonNetwork.IsConnected) {
+            //Set app version before connecting
+            PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion = gameVersion;
+            //Connect to instance of Photon Cloud Network
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    //Checks if player was connected / disconnected
+    // - If player was connected make player join random room
+    // - If not, send a message/cause
+    public override void OnConnectedToMaster () {
+        Debug.Log("OnConnectedToMaster Called");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected (Photon.Realtime.DisconnectCause cause) {
+        Debug.LogWarningFormat("OnDisconnected Called", cause);
+    }
+
+    public override void OnRoomListUpdate (List<RoomInfo> roomList) {
+        Debug.Log("We have received the Room list");
+        //After this callback, update the room list
+        createdRooms = roomList;
+    }
+
+    void OnGUI () {
+        GUI.Window(0, new Rect(Screen.width / 2 - 450, Screen.height / 2 - 200, 900, 400), LobbyWindow, "Lobby");
+    }
+
+    void LobbyWindow(int index)
+    {
+        //Connection Status and Room creation Button
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label("Status: " + PhotonNetwork.NetworkClientState);
+
+        if (joiningRoom || !PhotonNetwork.IsConnected || PhotonNetwork.NetworkClientState != ClientState.JoinedLobby)
+        {
+            GUI.enabled = false;
+        }
+
+        GUILayout.FlexibleSpace();
+
+        //Room name text field
+        roomName = GUILayout.TextField(roomName, GUILayout.Width(250));
+
+        if (GUILayout.Button("Create Room", GUILayout.Width(125)))
+        {
+            if (roomName != "")
+            {
+                joiningRoom = true;
+
+                RoomOptions roomOptions = new RoomOptions();
+                roomOptions.IsOpen = true;
+                roomOptions.IsVisible = true;
+                roomOptions.MaxPlayers = (byte)maxPlayers; //Set any number
+
+                PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+            }
+        }
+
+        GUILayout.EndHorizontal();
+
+        //Scroll through available rooms
+        roomListScroll = GUILayout.BeginScrollView(roomListScroll, true, true);
+
+        if (createdRooms.Count == 0)
+        {
+            GUILayout.Label("No Rooms were created yet...");
+        }
+        else
+        {
+            for (int i = 0; i < createdRooms.Count; i++)
+            {
+                GUILayout.BeginHorizontal("box");
+                GUILayout.Label(createdRooms[i].Name, GUILayout.Width(400));
+                GUILayout.Label(createdRooms[i].PlayerCount + "/" + createdRooms[i].MaxPlayers);
+
+                GUILayout.FlexibleSpace();
+
+                if (GUILayout.Button("Join Room"))
+                {
+                    joiningRoom = true;
+
+                    //Set our Player name
+                    PhotonNetwork.NickName = playerName;
+
+                    //Join the Room
+                    PhotonNetwork.JoinRoom(createdRooms[i].Name);
+                }
+                GUILayout.EndHorizontal();
+            }
+        }
+
+        GUILayout.EndScrollView();
+
+        //Set player name and Refresh Room button
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label("Player Name: ", GUILayout.Width(85));
+        //Player name text field
+        playerName = GUILayout.TextField(playerName, GUILayout.Width(250));
+
+        GUILayout.FlexibleSpace();
+
+        GUI.enabled = (PhotonNetwork.NetworkClientState == ClientState.JoinedLobby || PhotonNetwork.NetworkClientState == ClientState.Disconnected) && !joiningRoom;
+        if (GUILayout.Button("Refresh", GUILayout.Width(100)))
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                //Re-join Lobby to get the latest Room list
+                PhotonNetwork.JoinLobby(TypedLobby.Default);
+            }
+            else
+            {
+                //We are not connected, estabilish a new connection
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+
+        GUILayout.EndHorizontal();
+
+        if (joiningRoom)
+        {
+            GUI.enabled = true;
+            GUI.Label(new Rect(900 / 2 - 50, 400 / 2 - 10, 100, 20), "Connecting...");
+        }
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) {
+        Debug.Log("OnCreateRoomFailed got called. This can happen if the room exists (even if not visible). Try another room name.");
+        joiningRoom = false;
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("OnCreatedRoom");
+        //Set our player name
+        PhotonNetwork.NickName = playerName;
+    }
+
+    //Checks if player was able to join room
+    // - If not, create the room by setting room options' max players
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("JoinRandomFailed");
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(PhotonNetwork.CountOfPlayers);
+        //Load the Scene called GameLevel (Make sure it's added to build settings)
+        if (PhotonNetwork.CountOfPlayers >= 1)
+        {
+            PhotonNetwork.LoadLevel("Maze");
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom Called");
+    }  
 }
