fabbd8a7c7aeba8bb62b07807f2d07027599040c
Eric Tong <eric.tong105@gmail.com>
2020-08-11 23:21:07 -0400

    Commit

---
+++ Assets/Multiplayer Scripts/PUN2_RoomController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+public class PUN2_RoomController : MonoBehaviourPunCallbacks
+{
+
+    //Player instance prefab, must be located in the Resources folder
+    public GameObject playerPrefab;
+
+    //Player spawn point
+    public Transform[] _spawnPoint;
+
+    public double lastTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        //In case we started this demo with the wrong scene being active, simply load the menu scene
+        if (PhotonNetwork.CurrentRoom == null)
+        {
+            Debug.Log("Is not in the room, returning back to Lobby");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Multiplayer");
+            return;
+        }
+
+        PhotonNetwork.Instantiate(playerPrefab.name, _spawnPoint[PhotonNetwork.LocalPlayer.ActorNumber - 1 % 2].transform.position, _spawnPoint[PhotonNetwork.LocalPlayer.ActorNumber - 1].transform.rotation);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (PhotonNetwork.CurrentRoom == null)
+            return;
+
+        //Leave this Room
+        // if (GUI.Button(new Rect(5, 5, 125, 25), "Leave Room")) {
+        //     PhotonNetwork.LeaveRoom();
+        // }
+
+        //Show the Room name
+        GUI.Label(new Rect(135, 5, 200, 25), PhotonNetwork.CurrentRoom.Name);
+
+        //Show the list of the players connected to this Room
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            //Show if this player is a Master Client. There can only be one Master Client per Room so use this to define the authoritative logic etc.)
+            string isMasterClient = (PhotonNetwork.PlayerList[i].IsMasterClient ? ": MasterClient" : "");
+            GUI.Label(new Rect(5, 35 + 30 * i, 200, 25), PhotonNetwork.PlayerList[i].NickName + isMasterClient);
+        }
+    }
+
+    public static string getNickname()
+    {
+        return PhotonNetwork.NickName;
+    }
+
+    public override void OnLeftRoom()
+    {
+        //We have left the Room, return back to the GameLobby
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameLobby");
+    }
+
+    public static void leaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+}
